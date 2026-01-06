40e271d79e78c386828ae33bd067de40b50ef7d0
suraj766325 <suraj766325@gmail.com>
2026-01-07 01:05:55 +0530

    changed by suraj

--- src/component/SubjectTabs.jsx
+++ src/component/SubjectTabs.jsx
@@ -1,20 +1,54 @@
+// import { NavLink } from "react-router-dom";
+
+// const subjects = ["subject1", "subject2", "subject3", "subject4", "subject5"];
+
+// function SubjectTabs() {
+//   return (
+//     <div className="flex flex-wrap justify-center gap-2 mb-6">
+//       {subjects.map((sub) => (
+//         <NavLink
+//           key={sub}
+//           to={`/assignments/${sub}`}
+//           className={({ isActive }) =>
+//             `px-3 py-1.5 rounded-full font-semibold text-sm transition 
+//              ${isActive ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-700"}`
+//           }
+//         >
+//           {sub.toUpperCase()}
+//         </NavLink>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default SubjectTabs;
+
+
+
+
 import { NavLink } from "react-router-dom";
 
-const subjects = ["subject1", "subject2", "subject3", "subject4", "subject5"];
+const subjects =[
+  {name:"ICP",route:"subject1"},
+  {name:"DM",route:"subject2"},
+  {name:"UHV",route:"subject3"},
+  {name:"UPM",route:"subject4"},
+  {name:"CALCULUS",route:"subject5"},
 
+  ]
 function SubjectTabs() {
   return (
     <div className="flex flex-wrap justify-center gap-2 mb-6">
       {subjects.map((sub) => (
         <NavLink
-          key={sub}
-          to={`/assignments/${sub}`}
+          key={sub.route}
+          to={`/assignments/${sub.route}`}
           className={({ isActive }) =>
             `px-3 py-1.5 rounded-full font-semibold text-sm transition 
              ${isActive ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-700"}`
           }
         >
-          {sub.toUpperCase()}
+          {sub.name.toUpperCase()}
         </NavLink>
       ))}
     </div>
